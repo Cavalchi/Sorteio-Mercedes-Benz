abfb58ea68209d03f1da1767103629bde8d96165
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-13 14:46:49 -0300

    Update att.js

--- Cadastro/att.js
+++ Cadastro/att.js
@@ -4,7 +4,7 @@ const path = 'Sorteio-Mercedes-Benz/Cadastro/att.js';
 const message = 'mensagem de confirmação';
 const token = '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';
 const messageElement = document.createElement('div');
-document.body.appendChild(messageElement);  
+document.body.appendChild(messageElement);
 
 // Obter o conteúdo atual do arquivo
 fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
@@ -16,18 +16,31 @@ fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
 .then(data => {
   // Decodificar o conteúdo do arquivo de base64
   let content = atob(data.content);
+
+  // Coletar os valores dos campos do formulário
+  let email = document.getElementById('email').value;
+  let name = document.getElementById('name').value;
+  let lastname = document.getElementById('lastname').value;
+  let phone = document.getElementById('phone').value;
+
+  // Formatar os dados do formulário
+  let formData = `E-mail: ${email}\nNome: ${name}\nSobrenome: ${lastname}\nTelefone: ${phone}`;
+
   // Adicionar os novos dados do formulário
-  content += '\nnovos dados do formulário';
+  content += `\n${formData}`;
+
   // Codificar o conteúdo atualizado em base64
   content = btoa(content);
+
   // Atualizar o conteúdo do arquivo
-return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
-  method: 'PUT',
-  headers: {
-    'Authorization': `token ${token}`,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ message, content })
+  return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `token ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ message, content })
+  });
 })
 .then(response => {
   if (response.ok) {
@@ -41,4 +54,4 @@ return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
 .catch(error => {
   // Exibir uma mensagem de erro
   messageElement.textContent = 'Ocorreu um erro ao salvar os dados.';
-});})
+});
