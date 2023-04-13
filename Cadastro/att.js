eeb915bb88b64d8ba2e0a679510903e1b9ff9941
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-13 14:44:39 -0300

    Update att.js

--- Cadastro/att.js
+++ Cadastro/att.js
@@ -2,7 +2,9 @@ const owner = 'Cavalchi';
 const repo = 'Sorteio-Mercedes-Benz';
 const path = 'Sorteio-Mercedes-Benz/Cadastro/att.js';
 const message = 'mensagem de confirmação';
-const token = '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';
+const token = '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';
+const messageElement = document.createElement('div');
+document.body.appendChild(messageElement);  
 
 // Obter o conteúdo atual do arquivo
 fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
@@ -19,15 +21,24 @@ fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
   // Codificar o conteúdo atualizado em base64
   content = btoa(content);
   // Atualizar o conteúdo do arquivo
-  return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `token ${token}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ message, content })
-  });
+return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+  method: 'PUT',
+  headers: {
+    'Authorization': `token ${token}`,
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ message, content })
 })
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(error => console.error(error));
+.then(response => {
+  if (response.ok) {
+    // Exibir uma mensagem de sucesso
+    messageElement.textContent = 'Dados salvos com sucesso!';
+  } else {
+    // Exibir uma mensagem de erro
+    messageElement.textContent = 'Ocorreu um erro ao salvar os dados.';
+  }
+})
+.catch(error => {
+  // Exibir uma mensagem de erro
+  messageElement.textContent = 'Ocorreu um erro ao salvar os dados.';
+});})
