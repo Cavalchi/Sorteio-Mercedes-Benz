4e5b78cc7065413b52d65963aeedb7ed59345d8e
Cavalchi <108312374+Cavalchi@users.noreply.github.com>
2023-04-16 00:04:33 -0300

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -117,6 +117,17 @@ form.addEventListener('submit', function(event) {
   // Coletar os dados do formulário
   let formData = new FormData(form);
   // Enviar os dados para o back4app usando uma solicitação HTTP POST
+  var teste = fetch('https://parseapi.back4app.com/classes/Person', {
+    method: 'GET',
+  headers: {
+    'X-Parse-Application-Id': 'YX1d8JAZy0FLhG22LfaS7gpibFwDOxTgAlpZcM2J',
+    'X-Parse-REST-API-Key': 'EXeHValff9JzcDdNNqE0ToicNT11fccveKXF4bOW',
+    'Content-Type': 'application/json'
+  }
+})
+.then(response => response.text())
+.then(result => console.log('TESTE' + result))
+.catch(error => console.error(error));
   fetch('https://parseapi.back4app.com/classes/Person', {
     method: 'POST',
   headers: {
